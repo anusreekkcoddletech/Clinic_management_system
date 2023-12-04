8ca364fb9e248eadd21c8ea6485f71e08e2e7fac
anusree.kk <anusree.kk@coddletech.com>
2023-12-04 09:16:47 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const authRoute = require('./routes/authRoute')
+const authRoute = require('./routes/auth')
 const PORT = 3001
 
 app.use(express.json())
@@ -9,3 +9,4 @@ app.use('/user', authRoute);
 app.listen(PORT,function () {
     console.log(`Server running at:http://localhost:${PORT}/`)
 })
+
--- models/auth.js
+++ models/auth.js
@@ -1,7 +1,7 @@
 const database = require('../databaseConnect');
-function registerUser(name, username, password, age, gender, phone, bloodgroup, callback) {
-  const qr = 'insert into patients (name, username, password, age, gender, phone, bloodgroup) values (?, ?, ?, ?, ?, ?, ?)'
-  const values = [name, username, password, age, gender, phone, bloodgroup]
+function registerUser(name, username, password, age, gender, phone, bloodgroup,employees_id, callback) {
+  const qr = 'insert into patients (name, username, password, age, gender, phone, bloodgroup,employees_id) values (?, ?, ?, ?, ?, ?, ?,?)'
+  const values = [name, username, password, age, gender, phone, bloodgroup,employees_id]
   database.query(qr, values, callback)
 }
 function loginUser(username, password, callback) {
@@ -14,4 +14,3 @@ module.exports = {
   registerUser,
   loginUser,
 }
-
