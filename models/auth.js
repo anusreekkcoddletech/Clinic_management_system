14208dbef7cee21f997c07fd7f135674dbb37b7a
anusree.kk <anusree.kk@coddletech.com>
2023-12-07 01:48:22 +0530

    Code updated

--- models/auth.js
+++ models/auth.js
@@ -1,49 +1,56 @@
 const mysql = require('mysql')
-const con = require('../databaseConnect')
+const {makeDb} = require('../databaseConnect')
 const util = require('util')
-con.connect()
 
 async function registerUser(name, username, password, age, gender, phone, bloodgroup, employees_id) {
+  const db = makeDb()
   try {
     const qr = 'insert into patients (name, username, password, age, gender, phone, bloodgroup,employees_id) values (?, ?, ?, ?, ?, ?, ?,?)'
     const values = [name, username, password, age, gender, phone, bloodgroup, employees_id]
-    await query(qr, values)
+    await query(db, qr, values)
   } catch (err) {
     console.error('Error:', err.message)
   }
+  finally {
+    await close(db)
+  }
 }
-async function checkRegisteredUser(username){
-  try{
+async function checkRegisteredUser(username) {
+  const db = makeDb()
+  try {
     const qr = 'select * from patients where username =?'
-    const values=[username]
-    const registeredUser =await query(qr, values)
-      return registeredUser
-  }catch(err){
-    console.error('Error checking existing user:',err.message)
+    const values = [username]
+    const registeredUser = await query(db, qr, values)
+    return registeredUser
+  } catch (err) {
+    console.error('Error checking existing user:', err.message)
+  } finally {
+    await close(db)
   }
 }
-  async function loginUser(username, password) {
-    try {
-      const qr = 'select * from patients where username=? and password=?'
-      const values = [username, password]
-      const login = await query(qr, values)
-      return login
-    }catch (err) {
-      console.error('Error:', err.message)
-    }
+async function loginUser(username, password) {
+  const db = makeDb()
+  try {
+    const qr = 'select * from patients where username=? and password=?'
+    const values = [username, password]
+    const login = await query(db, qr, values)
+    return login
+  } catch (err) {
+    console.error('Error:', err.message)
+  } finally {
+    await close(db)
   }
-   function query(qr, values) {
-    return util.promisify(con.query).call(con, qr, values)
-  }
-    function close() {
-    return util.promisify(con.end).call(con)
-  }
+}
+function query(db, qr, values) {
+  return util.promisify(db.query).call(db, qr, values)
+}
+function close(db) {
+  return util.promisify(db.end).call(db)
+}
 module.exports = {
   registerUser,
   checkRegisteredUser,
-   loginUser,
-   query,
-   close
+  loginUser,
+  query,
+  close
 }
-
-
