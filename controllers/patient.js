8fbda2ffac3799847d9294bddebbba3967e390b4
anusree.kk <anusree.kk@coddletech.com>
2023-12-12 12:54:50 +0530

    added pagination in code

--- controllers/patient.js
+++ controllers/patient.js
@@ -2,16 +2,14 @@ const userModel = require('../models/patient')
 
 const getCurrentMonthPatients = async (req, res) => {
     try {
-        const { page } = req.query
-        const parsedPage = parseInt(page, 10) || 1
+        const { page,limit } = req.query
 
-        const limit = 2
-        const currentMonthPatients = await userModel.getCurrentMonthPatients(parsedPage, limit)
+        const currentMonthPatients = await userModel.getCurrentMonthPatients(page, limit)
         console.log(currentMonthPatients)
-
-        res.status(200).send({ currentMonthPatients })
-    } catch (error) {
-        console.error('Error fetching data:', error.message)
+       
+        res.status(200).send( { success: true, message: 'The current month patients listed below', data: currentMonthPatients})
+    } catch (err) {
+        console.log('Error fetching data:', err.message)
         res.status(500).send({ error: 'Failed to fetch data' })
     }
 }
