9722fba159c83503a681e1468702667e4739e072
anusree.kk <anusree.kk@coddletech.com>
2023-12-13 11:55:26 +0530

    api created for patientslist by date

--- models/patient.js
+++ models/patient.js
@@ -22,17 +22,15 @@ async function getSelectedMonthPatients(month, year) {
     try {
         if (!month || !year) {
 
-        const qr = `SELECT * FROM patients WHERE MONTH(created) = MONTH(CURRENT_DATE()) 
-        AND YEAR(created) = YEAR(CURRENT_DATE())`
+            const qr = `SELECT * FROM patients WHERE MONTH(created) = MONTH(CURRENT_DATE()) 
+            AND YEAR(created) = YEAR(CURRENT_DATE())`
 
             const currentMonthPatients = await db.query(qr)
             return currentMonthPatients
         }
-         else {
-
+        else {
             const qr1 = `SELECT * FROM patients WHERE MONTH(created) = ${month} AND  YEAR(created)  = ${year}`
             const selectedMonthPatients = await db.query(qr1)
-            console.log(selectedMonthPatients)
             return selectedMonthPatients
         }
     } catch (err) {
@@ -41,9 +39,62 @@ async function getSelectedMonthPatients(month, year) {
         await db.close()
     }
 }
+
+
+async function getPatientsbyDate(yesterday, today, tomorrow) {
+    const db = makeDb()
+    try {
+        if (yesterday && today && tomorrow) {
+            const qr = `SELECT * FROM patients WHERE  DATE(created) IN (CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY),DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) )ORDER BY created`
+            const patientsbyDate = await db.query(qr)
+            return patientsbyDate
+        } 
+        else if ( yesterday  && !today &&  tomorrow) {
+            const qr1 = `SELECT * FROM patients WHERE DATE(created) IN ( DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY),DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))`
+            const patientsbyDate = await db.query(qr1)
+            return patientsbyDate
+        } 
+        else if ( yesterday && today && !tomorrow)  {
+            const qr2 = `SELECT * FROM patients WHERE DATE(created) IN (CURRENT_DATE(),DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))`
+            const patientsbyDate = await db.query(qr2)
+            return patientsbyDate
+        }
+         else if (!yesterday && today && tomorrow  ) {
+            const qr3 = `SELECT * FROM patients WHERE DATE(created) IN (CURRENT_DATE(), DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY))`
+            const patientsbyDate = await db.query(qr3)
+            return patientsbyDate
+        }
+         else if ( yesterday && !today && !tomorrow ) {
+            const qr4 = `SELECT * FROM patients WHERE DATE(created) =DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)`
+            const patientsbyDate = await db.query(qr4)
+            return patientsbyDate
+        }
+        else if ( !yesterday && today && !tomorrow  ) {
+            const qr5 = `SELECT * FROM patients WHERE DATE(created) =CURRENT_DATE()`
+            const patientsbyDate = await db.query(qr5)
+            return patientsbyDate
+        }
+        else if ( !yesterday && !today && tomorrow  ) {
+            const qr6 = `SELECT * FROM patients WHERE DATE(created) = DATE_ADD(CURRENT_DATE(), INTERVAL 1 DAY)`
+            const patientsbyDate = await db.query(qr6)
+            return patientsbyDate
+        }
+        else if ( !yesterday && !today && !tomorrow  ) {
+            const qr7 = `SELECT * FROM patients`
+            const selectedPatients = await db.query(qr7)
+            return selectedPatients
+        }
+ 
+    } catch (err) {
+        console.log('Error fetching patients by date:', err.message)
+    } finally {
+        await db.close()
+    }
+}
 module.exports = {
     getCurrentMonthPatients,
-    getSelectedMonthPatients
+    getSelectedMonthPatients,
+    getPatientsbyDate
 }
 
 
